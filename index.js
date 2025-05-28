939f7e57ea14ab7ad635017ce6d8517edc6b300a
Surafel Ayalew <surafelayalew1216@gmail.com>
2025-05-28 14:42:46 -0400

    Updated index.js with teacher's starter code

--- index.js
+++ index.js
@@ -1,104 +1,164 @@
+// Necessary Imports, DO NOT REMOVE
+const { LinkedList } = require("./LinkedList");
+const { Student } = require('./Student')
 const readline = require('readline');
-const { Student } = require('./Student');
-const { StudentManagementSystem } = require('./StudentManagementSystem'); // your LinkedList logic
 
-const studentManagementSystem = new StudentManagementSystem();
-
+// Initialize terminal interface
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
-  prompt: '>'
+  output: process.stdout
 });
 
-rl.prompt();
+// Creates the Student Management System as a Linked List
+/**
+ * studentManagementSystem is the object that the main() function will be modifying
+ */
+const studentManagementSystem = new LinkedList();
 
-rl.on('line', async (input) => {
-  await handleCommand(input);
-  rl.prompt();
-});
+// Display available commands
+function main() {
+  console.log(`
+      Available Commands:
+      - add [name] [year] [email] [specialization]: Add a student
+      - remove [email]: Remove a student by email
+      - display: Show all students
+      - find [email]: Find a student by email
+      - save: Save the current linked list to the specified file
+      - load [fileName]: Load a linked list from a file
+      - clear: Clear the current linked list
+      - q: Quit the terminal
+  `);
+}
 
-// Your teacher's provided function (DO NOT CHANGE STRUCTURE OR COMMENTS)
+// Command handling logic
 async function handleCommand(command) {
   const [operation, ...args] = command.trim().split(' ');
 
   switch (operation) {
     case 'add':
-      console.log('Adding student...');
-      const [name, year, email, specialization] = args;
-      // --------> WRITE YOUR CODE BELOW
-      const newStudent = new Student(name, Number(year), email, specialization);
-      studentManagementSystem.addStudent(newStudent);
-      console.log('Current students:', studentManagementSystem.displayStudents());
-      // --------> WRITE YOUR CODE ABOVE
-      break;
+      /**
+       * TODO:
+       *  Finds a particular student by email, and returns their information
+       *  You will need to do the following:
+       *   - Implement LinkedList (run tests locally to check implementation)
+       *   - Grab the args (code is given)
+       *   - Use implemented functions in LinkedList to add the Student, and display the updated LinkedList
+       */
+        console.log('Adding student...')
+        const [name, year, email, specialization] = args
+        // --------> WRITE YOUR CODE BELOW
+
+        // --------> WRITE YOUR CODE ABOVE
+        break;
 
     case 'remove':
-      console.log('Removing student...');
+      /**
+       * TODO:
+       *  Removes a particular student by email
+       *  You will need to do the following:
+       *   - Implement LinkedList (run tests locally to check implementation)
+       *   - Grab the args (removeEmail)
+       *   - Use implemented functions in LinkedList to remove the Student, and display the updated LinkedList
+       */
+      console.log('Removing student...')
       // --------> WRITE YOUR CODE BELOW
-      const removeEmail = args[0];
-      studentManagementSystem.removeStudent(removeEmail);
-      console.log('Current students:', studentManagementSystem.displayStudents());
+      
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'display':
-      console.log('Displaying students...');
+      /**
+       * TODO:
+       *  Displays the students in the Linked List
+       *  You will need to do the following:
+       *   - Use implemneted functions in LinkedList to display the student
+       */
+      console.log('Displaying students...')
       // --------> WRITE YOUR CODE BELOW
-      const studentsList = studentManagementSystem.displayStudents();
-      if (studentsList) {
-        console.log(studentsList);
-      } else {
-        console.log('No students in the system.');
-      }
+
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'find':
-      console.log('Finding student...');
+      /**
+       * TODO:
+       *  Finds a particular student by email, and returns their information
+       *  You will need to do the following:
+       *   - Implement LinkedList (run tests locally to check implementation)
+       *   - Grab the args (findEmail)
+       *   - Use implemented functions in LinkedList to grab the Student
+       *   - Use implemented functions in Student to display if found, otherwise, state "Student does not exist"
+       */
+      console.log('Finding student...')
       // --------> WRITE YOUR CODE BELOW
-      const findEmail = args[0];
-      const foundStudent = studentManagementSystem.findStudent(findEmail);
-      if (foundStudent !== -1) {
-        console.log(foundStudent.getString());
-      } else {
-        console.log('Student does not exist');
-      }
+      
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'save':
-      console.log('Saving data...');
+      /**
+       * TODO:
+       *  Saves the current LinkedList to a specified JSON file
+       *  You will need to do the following:
+       *   - Implement LinkedList (run tests locally to check implementation)
+       *   - Grab the args (saveFileName)
+       *   - Use implemented functions in LinkedList to save the data
+       */
+      console.log('Saving data...')
       // --------> WRITE YOUR CODE BELOW
-      const saveFileName = args[0];
-      await studentManagementSystem.saveToJson(saveFileName);
-      console.log(`Data saved to ${saveFileName}`);
-      // --------> WRITE YOUR CODE ABOVE
-      break;
 
-    case 'load':
-      console.log('Loading data...');
+      // --------> WRITE YOUR CODE ABOVE
+
+    case "load":
+      /**
+       * TODO:
+       *  Loads data from specified JSON file into current Linked List
+       *  You will need to do the following:
+       *   - Implement LinkedList (run tests locally to check implementation)
+       *   - Grab the args (loadFileName)
+       *   - Use implemented functions in LinkedList to save the data, and display the updated LinkedList
+       */
+      console.log('Loading data...')
       // --------> WRITE YOUR CODE BELOW
-      const loadFileName = args[0];
-      await studentManagementSystem.loadFromJSON(loadFileName);
-      console.log('Current students:', studentManagementSystem.displayStudents());
+
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'clear':
-      console.log('Clearing data...');
+      /**
+       * TODO:
+       *  Clears all data in the Linked List
+       *  You will need to do the following:
+       *   - Implement LinkedList (run tests locally to check implementation)
+       *   - Use implemented functions in LinkedList to clear the data
+       */
+      console.log('Clearing data...')
       // --------> WRITE YOUR CODE BELOW
-      studentManagementSystem.clearStudents(); // fixed: now a public method
-      console.log('All students cleared.');
+
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'q':
-      console.log('Exiting...');
-      rl.close();
-      break;
+        console.log('Exiting...');
+        rl.close();
+        break;
 
     default:
-      console.log('Unknown command. Type "help" for a list of commands.');
-      break;
+        console.log('Unknown command. Type "help" for a list of commands.');
+        break;
   }
 }
+
+// Start terminal-based interaction (DO NOT MODIFY)
+console.log('Welcome to the Student Management System!');
+main();
+rl.on('line', async (input) => {
+  if (input.trim().toLowerCase() === 'help') {
+    main();
+  } else {
+      await handleCommand(input);
+  }
+});
+rl.on('close', () => {
+  console.log('Goodbye!');
+});
